8e1d7827b7a09ed784663aa9a7354bc3f5051f0e
Minh Tâm <72743180+Minh-Tam27@users.noreply.github.com>
2021-03-29 11:19:19 +0700

    done ss2

---
+++ C4EJS-2/index.js
@@ -0,0 +1,22 @@
+//If - else -if -....
+let age = prompt('Your age?');
+if (age < 10)
+{
+    alert ('Stay at home');
+}
+else if (age < 18)
+{
+    alert ('Eat ice cream');
+}
+else
+{
+    alert ('Go to cinema');
+}
+//for
+for(let i=0; i<=3; i++){
+  console.log(i);
+}
+//while
+while(true) {
+  console.log('This is while');
+}
